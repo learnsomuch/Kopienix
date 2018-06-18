014282ab51945532446f4fb919f1cfc4ba5f6bf7
learnsomuch <sankar@learnsomuch.com>
2018-06-18 21:02:27 +0000

    added reference to whoami.c

--- src/whoami.c
+++ src/whoami.c
@@ -1,6 +1,6 @@
 /* Header files
    stdio.h - For output of whoami command
-   pwd.h - For passwd structure with uid_t and for struct passwd */
+   pwd.h - For passwd structure with uid_t and for struct passwd (Refer: http://pubs.opengroup.org/onlinepubs/7908799/xsh/pwd.h.html) */
 #include <stdio.h>
 #include <pwd.h>
 
