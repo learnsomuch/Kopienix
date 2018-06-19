3301286f700e6d34c68b7c367fb53d46d570ad3c
learnsomuch <sankar@learnsomuch.com>
2018-06-19 18:24:45 +0000

    Added more options to id

--- src/id.c
+++ src/id.c
@@ -19,12 +19,32 @@ int main(int argc, char *argv[]) {
   	/* get passwd uid */
   	pw = getpwuid(uid);
 
-  	/* Check if pw is not null */
-  	if (pw != NULL) {
-        	/* Fetch pw_uid, pw_name and gid from struct of pw */
+	if(argv[1] == NULL) {
+
+		/* Default print - uid with uid number and user, group id with username */
 		printf("uid=%d(%s), gid=%d(%s)\n", pw->pw_uid, pw->pw_name, pw->pw_gid, pw->pw_name);
-  	}
-  
+
+	} else if((strcmp("-f", argv[1]) == 0) || (strcmp("--full-name", argv[1]) == 0)){
+		
+		/* Print full name */
+		printf("Full Name: %s\n", pw->pw_gecos);
+	
+	} else if((strcmp("-g", argv[1]) == 0) || (strcmp("--group-id", argv[1]) == 0)) {
+		
+		/* Print group id */
+		printf("Group id: %d\n", pw->pw_gid);
+
+	} else if((strcmp("-u", argv[1]) == 0) || (strcmp("--user-id", argv[1]) == 0)) {
+		
+		/* Print user id */
+		printf("User id: %d\n", pw->pw_uid);
+
+	}else if((strcmp("-l", argv[1]) == 0) || (strcmp("--login-name", argv[1]) == 0)) {
+		
+		/* Print user login name */
+		printf("Login Name: %s\n", pw->pw_name);
+	}
+ 
   	/* Success return code */
   	return 0;
 }
