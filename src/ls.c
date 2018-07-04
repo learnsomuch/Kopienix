d2d2e36b1f3bd7fb87516b217eec0c980e54dd3a
learnsomuch <sankar@learnsomuch.com>
2018-07-04 21:21:45 +0000

    Implementation if ls without dots

--- src/ls.c
+++ src/ls.c
@@ -4,15 +4,26 @@
 #include <stdio.h>
 #include <dirent.h>
 
+/* Pass empty args with no return code */
 void main(){
+
 	/* Variables */ 
 	struct dirent *d = NULL;
-	
+
+	/* Opens directory in current location */	
 	DIR *di = opendir(".");
 
+	/* if directory is not null */
 	while((d = readdir(di)) != NULL) {
-		printf("%s\n", d->d_name);
+		
+		/* Check if the directory has . or .. then do nothing else print details */
+		if((strcmp(d->d_name, ".") == 0) || (strcmp(d->d_name, "..") == 0)) {
+			// Do nothing	
+		} else {
+			printf("%s\n", d->d_name);
+		}
 	}
-	
+
+	/* close the opened directory */
 	closedir(di);
 }
