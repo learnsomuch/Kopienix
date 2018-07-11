8386e24a6a2af14c1f8c4b1e31cfcf546379007e
learnsomuch <sankar@learnsomuch.com>
2018-07-11 20:41:51 +0000

    Added time, formatting in uptime.c

--- src/uptime.c
+++ src/uptime.c
@@ -1,8 +1,10 @@
 /* Header Files 
    stdio.h - For output 
-   sys/sysinfo.h - For system information such as load */
+   sys/sysinfo.h - For system information such as load
+   time.h - for time and ctime */
 #include <stdio.h>
 #include <sys/sysinfo.h>
+#include <time.h>
 
 /* Main Function */
 int main() {
@@ -11,8 +13,11 @@ int main() {
 	   Reference: http://man7.org/linux/man-pages/man2/sysinfo.2.html */
 	struct sysinfo sinfo;
 	int days, hours, mins;
+	time_t t = time(NULL);
 
-	
+        /* Return struct. In this case, &t is datatype of time_t. Reference to localtime and struct tm: http://pubs.opengroup.org/onlinepubs/7908799/xsh/time.h.html */
+        struct tm *string_time = localtime(&t);
+
 	if(sysinfo(&sinfo) == 0) {
 	
 		/* Converstions to readable format */
@@ -21,7 +26,7 @@ int main() {
   		mins = (sinfo.uptime / 60) - (days * 1440) - (hours * 60);
 
 		/* Print details to screen */
-		printf("Uptime: %u days, %dhours, %dminutes, %ldseconds\n", days, hours, mins, sinfo.uptime % 60);
+		printf("%02d:%02d:%02d up %u days, %dh:%dm:%lds\n",string_time->tm_hour, string_time->tm_min, string_time->tm_sec, days, hours, mins, sinfo.uptime % 60);
 
 		/* Success return code */
 		return 0;	
