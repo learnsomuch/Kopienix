def2ca53e6fd08b264093bba86f355880d49d894
learnsomuch <sankar@learnsomuch.com>
2018-07-18 20:33:33 +0000

    free.c shared + buffers details, output optimization

--- src/free.c
+++ src/free.c
@@ -10,6 +10,7 @@ int help() {
 	/* Print supported options */
 	printf("Usage: free [OPTION]...\n");
 	printf("Possible supported OPTION:\n");
+	printf("\t-b, --B \t\t Print memory details in bytes\n");
 	printf("\t-k, --KB \t\t Print memory details in kilobytes\n");
 	printf("\t-m, --MB \t\t Print memory details in megabytes\n");
 	printf("\t-m, --GB \t\t Print memory details in gigabytes\n");
@@ -19,6 +20,19 @@ int help() {
 	
 	/* Exit with return code 1 */
 	return 1;
+}
+
+/* print to screen */
+void screen(unsigned long tram, unsigned long fram, unsigned long sram, unsigned long bram) {
+
+	printf("Memory Information\n");
+	printf("******************\n");
+	printf("total: %zu\n", tram);
+	printf("used: %zu\n", tram - fram);
+	printf("free: %zu\n", fram);
+	printf("shared: %zu\n", sram);
+	printf("buffers: %zu\n", bram); 
+
 }
 
 int main(int argc, char *argv[]) {
@@ -33,51 +47,55 @@ int main(int argc, char *argv[]) {
   	static int MB = 1024 * 1024;
   	static int KB = 1024;
 
-	unsigned long tram, fram;
+	unsigned long tram, fram, sram, bram;
 				
 	if(sysinfo(&sinfo) == 0) {
 	
 		/* Get data from sinfo */
 		tram = sinfo.totalram;
 		fram = sinfo.freeram;	
-		
-		if(argv[1] == NULL) {	
+		sram = sinfo.sharedram;
+		bram = sinfo.bufferram;
 	
-			printf("         total         used \t available\n");
-                        printf("Mem: %zu    %zu       %zu\n", tram, tram - fram, fram);
+		if((argv[1] == NULL) || ((strcmp("-b", argv[1]) == 0) || (strcmp("--B", argv[1]) == 0))) {	
+
+			screen(tram, fram, sram, bram);	
 
 		} else if((strcmp("-k", argv[1]) == 0) || (strcmp("--KB", argv[1]) == 0)) {
                         
                         tram = tram / KB;
 			fram = fram / KB;
+			sram = sram / KB;
+			bram = bram / KB;
 
-			printf("         total         used \t available\n");
-                        printf("Mem:    %zu       %zu          %zu\n", tram, tram - fram, fram);			
+			screen(tram, fram, sram, bram);
 
                 } else if((strcmp("-m", argv[1]) == 0) || (strcmp("--MB", argv[1]) == 0)) {
 		
 			tram = tram / MB;
 			fram = fram / MB;
+			sram = sram / MB;
+                        bram = bram / MB;
 
-			printf("         total         used \t available\n");
-			printf("Mem:       %zu          %zu          %zu\n", tram, tram - fram, fram);
-			
+			screen(tram, fram, sram, bram);	
 
 		} else if((strcmp("-g", argv[1]) == 0) || (strcmp("--GB", argv[1]) == 0)) {
                 
 		        tram = tram / GB;
 			fram = fram / GB;
+			sram = sram / GB;
+                        bram = bram / GB;
 
-			printf("         total         used \t available\n");
-                        printf("Mem:       %zu             %zu             %zu\n", tram, tram - fram, fram);
+			screen(tram, fram, sram, bram);
 
 		} else if(strcmp("--TB", argv[1]) == 0) {
 
                         tram = tram / TB;
 			fram = fram / TB;
+			sram = sram / TB;
+                        bram = bram / TB;
 
-			printf("         total         used \t available\n");
-                        printf("Mem:       %zu             %zu             %zu\n", tram, tram - fram, fram);
+			screen(tram, fram, sram, bram);
 
                 } else {
 
