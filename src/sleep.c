7054e66a038ac0805a587f0c159b3618af99411f
learnsomuch <sankar@learnsomuch.com>
2018-07-27 16:29:22 +0000

    sleep.c help function

--- src/sleep.c
+++ src/sleep.c
@@ -1,22 +1,42 @@
 /* Header files
    stdio.h - for input and output 
+   stdlib.h - for strtol
+   ctype.h - for isdigit
    unistd.h - For sleep */
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+/* Helper function */
+int help() {
+
+	/* Print supported options */
+	printf("Usage: sleep [OPTION]...\n");
+	printf("Possible supported OPTION:\n");
+	printf("\t<number if seconds> \t Enter number of seconds you want to sleep\n");
+	
+	/* Exit with return code 1 */
+	return 1;
+}
 
 /* Main Function */
 int main(int argc, char *argv[]) {
-
-	char *p;
+	
+	char *p = NULL;
 
 	/* Check if variable is empty or not */
-	if(argv[1] != NULL){
-		
-		/* Sleep function via stdlib and convert char to number */
-		sleep(strtol(argv[1], &p, 10));
-	}
+	if(*p != '\0') {
 
+		return help();
+	} if(argv[1] == NULL) {
+
+		return help();
+	} else {
+
+		/* Sleep function via stdlib and convert char to number */
+               	sleep(strtol(argv[1], &p, 10));
+	}
 	/* Success return code */
 	return 0;
 }
