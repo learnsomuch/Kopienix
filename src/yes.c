44fcf250c7d061898a07d8decb5165212840c320
learnsomuch <sankar@learnsomuch.com>
2018-08-01 17:48:20 +0000

    yes.c initial commit

---
+++ src/yes.c
@@ -0,0 +1,20 @@
+/* Header files 
+   stdio.h - for input and output */
+#include <stdio.h>
+
+int main() {
+
+	/* Variables */
+	char yes = 'y';
+
+	/* While loop is forever */
+	while(1) {
+		
+		/* Print char on screen */
+		printf("%c\n", yes);
+	}
+
+	/* Success return code */
+	return 0;
+
+}
